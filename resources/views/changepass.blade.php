929f3229b11a612962d7730427e08b71f4ff3747
MuhammadRezaPahlevi0218 <reyzae99@gmail.com>
2021-05-31 23:06:44 +0700

    mengatur front end ganti password

--- resources/views/changepass.blade.php
+++ resources/views/changepass.blade.php
@@ -3,36 +3,66 @@
 @section('title', 'Profile')
 
 @section('content_header')
-<form method="post">
-    {{ csrf_field() }}
-    <div class="input-group mb-3">
-        <input type="password" name="password" class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}" placeholder="{{ __('adminlte::adminlte.password') }}">
-        <div class="input-group-append">
-            <div class="input-group-text">
-                <span class="fas fa-lock {{ config('adminlte.classes_auth_icon', '') }}"></span>
+<!doctype html>
+<html lang="en" class="fullscreen-bg">
+
+<body>
+    <!-- WRAPPER -->
+    <div id="wrapper">
+        <div class="vertical-align-wrap">
+            <div class="vertical-align-middle">
+                <div class="auth-box ">
+                    <div class="left">
+                        <div class="content">
+                            <div style="text-align: center;">
+                                <h3>Ganti Password</h3>
+                            </div>
+                            <form method="POST" action="">
+                                @csrf
+
+                                <div class="form-group row">
+                                    <label for="oldpassword" class="col-md-4 col-form-label text-md-right">{{ __('Old Password') }}</label>
+
+                                    <div class="col-md-6">
+                                        <input  type="oldpassword" class="form-control" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label for="password " class="col-md-4 col-form-label text-md-right">{{ __('New Password') }}</label>
+
+                                    <div class="col-md-6">
+                                        <input id="password" type="newpassword" class="form-control " name="password" required autocomplete="current-password">
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Retype New Password') }}</label>
+
+                                    <div class="col-md-6">
+                                        <input id="password" type="retype" class="form-control " name="password" required autocomplete="current-password">
+                                    </div>
+                                </div>
+
+                                <div class="form-group row mb-0">
+                                    <div class="col-md-8 offset-md-4">
+                                        <button type="submit" class="btn btn-primary">
+                                            {{ __('Submit') }}
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+
+                    <div class="clearfix"></div>
+                </div>
             </div>
         </div>
     </div>
-    <div class="input-group mb-3">
-        <input type="password" name="password" class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}" placeholder="{{ __('adminlte::adminlte.password') }}">
-        <div class="input-group-append">
-            <div class="input-group-text">
-                <span class="fas fa-lock {{ config('adminlte.classes_auth_icon', '') }}"></span>
-            </div>
-        </div>
-    </div>
-    <div class="input-group mb-3">
-        <input type="password" name="password" class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}" placeholder="{{ __('adminlte::adminlte.password') }}">
-        <div class="input-group-append">
-            <div class="input-group-text">
-                <span class="fas fa-lock {{ config('adminlte.classes_auth_icon', '') }}"></span>
-            </div>
-        </div>
-    </div>
-    <div class="col-5">
-        <div class="box-footer">
-            <button type="submit" class="btn btn-primary" title="Simpan Data"> <i class="glyphicon glyphicon-floppy-disk"></i> Simpan</button>
-        </div>
-    </div>
-</form>
+    <!-- END WRAPPER -->
+</body>
+
+</html>
 @stop
